cf758618161763ae31a80bdce1eadb33265a4415
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-11-16 18:40:12 +0100

    Routing setup complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+//Imports 
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
 
+//Main function 
 function App() {
   return (
     <div className="App">
-
+      {/* Routes */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes> 
     </div>
   );
 }
